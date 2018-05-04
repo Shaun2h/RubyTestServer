3ef7672eb175aad2903531419c63b37a5023645f
unknown <smurfination1@gmail.com>
2018-05-04 20:20:35 +0800

    minor note added. It's really tiring to do this with shit sleep cycle

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  #resources :urls
+  resources :urls
   get 'base/new', to: 'base#new'
   post 'base2', action: :do, controller: 'base'
+  root to: 'base#new' #set root to controller named base, with the new method...
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
